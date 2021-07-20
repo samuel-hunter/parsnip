b1098faf764d03e6131bb986c432aef81ce2108f
Samuel Hunter <samuel@shunter.xyz>
2021-07-19 19:31:48 -0700

    Setup backbone for parsing streams

--- parabeaker.lisp
+++ parabeaker.lisp
@@ -1,52 +1,118 @@
 (defpackage #:parabeaker
   (:use #:cl)
-  (:export))
+  (:export #:parse
+
+           #:accept-string
+           #:accept-char
+           #:accept-char-if
+           #:accept-charbag
+           #:parser-let
+           #:parser-any
+           #:parser-many
+           #:parser-many1
+           #:parser-or
+           #:parser-name))
 
 (in-package #:parabeaker)
 
 
 
-(defun parse (parser source)
+(defmacro TODO ()
+  `(error "Not implemented"))
+
+(defun parse (parser stream)
   "Run the given parser."
-  (declare (ignore parser source))
+  (declare (ignore parser stream))
   (TODO))
+
+(defclass result ()
+  ((value :initarg :value :reader result-value)
+   (position :initarg :position :reader result-position)))
+
+(defmethod print-object ((object result) stream)
+  (print-unreadable-object (object stream :type t)
+    (format stream "~S POS=~D"
+            (result-value object)
+            (result-position object))))
+
+(defclass just (result) ())
+(defclass expected (result) ())
+
+(defun just (value stream)
+  (make-instance 'just
+                 :value value
+                 :position (file-position stream)))
+
+(defun expected (value stream)
+  (make-instance 'expected
+                 :value value
+                 :position (file-position stream)))
+
+(defun flatmap-result (result function)
+  (etypecase result
+    (just (funcall function result))
+    (expected result)))
+
+(defun expectmap-result (result function)
+  (etypecase result
+    (just result)
+    (expected (funcall result function))))
 
 
 
-(defmacro TODO ())
-
 (defun accept-string (string)
   "Return a parser that accepts the given string value."
-  (declare (ignore string))
-  (TODO))
+  (lambda (stream)
+    (let ((actual-string (make-string (length string))))
+      ;; TODO deal with EOF here
+      (assert (= (length string)
+                 (read-sequence actual-string stream)))
+      (if (string= string actual-string)
+          (just actual-string stream)
+          (expected string stream)))))
 
 (defun accept-char (char)
   "Return a parser that accepts the given character value."
-  (declare (ignore char))
-  (TODO))
+  (lambda (stream)
+    ;; TODO deal with EOF here
+    (let ((actual-char (peek-char nil stream)))
+      (if (char= char actual-char)
+          (prog1 (just actual-char stream)
+            (read-char stream))
+          (expected char stream)))))
+
+(defun accept-char-if (predicate)
+  "Return a parser that accepts a character if the given predicate returns true."
+  (lambda (stream)
+    (let ((actual-char (peek-char nil stream)))
+      (if (funcall predicate actual-char)
+          (prog1 (just actual-char stream)
+            (read-char stream))
+          (expected predicate stream)))))
 
 (defmacro parser-let (bindings &body body)
-  "Return a parser that binds a new variable to a parser result in each binding, then returns the body."
+  "Return a parser that binds a new variable to a parser result in each
+   binding, then returns the body."
   (declare (ignore bindings body))
   (TODO))
 
-(defun any (&rest parsers)
+(defun parser-any (&rest parsers)
   "Return a parser that attempts each parser while no input is consumed, until
    one parser succeeds."
   (declare (ignore parsers))
   (TODO))
 
-(defun many (&rest parsers)
+(defun parser-many (&rest parsers)
   "Return a parser that keeps parsing the given parser until failure."
   (declare (ignore parsers))
   (TODO))
 
-(defun many1 (&rest parsers)
+(defun parser-many1 (&rest parsers)
   "Return a parser that keeps parsing the given parser until failure, at least once."
   (declare (ignore parsers))
   (TODO))
 
-(defun try (parser)
+(defun parser-try (parser)
   (declare (ignore parser))
   (TODO))
 
@@ -54,3 +120,7 @@
   "Wraps a name around a parser, so that errors are given a keyword what to expect."
   (declare (ignore name parser))
   (TODO))
+
+(defun accept-charbag (char-bag)
+  "Return a parser that accepts any character within the given char bag."
+  (parser-name char-bag (accept-char-if (lambda (c) (position c char-bag)))))
