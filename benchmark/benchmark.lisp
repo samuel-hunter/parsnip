4d123628c113b2e384aefbf6928df3612cd4cff8
Samuel Hunter <samuel@shunter.xyz>
2021-07-26 17:55:36 -0700

    Add from-string decoding to benchmark

--- benchmark/benchmark.lisp
+++ benchmark/benchmark.lisp
@@ -3,6 +3,7 @@
 ;;; Copyright 2021 Samuel Hunter <samuel (at) shunter (dot) xyz>
 ;;; BSD-3-Clause
 
+(require :alexandria)
 (require :cl-json)
 (require :parsnip/examples)
 
@@ -18,19 +19,33 @@
   (merge-pathnames #P"benchmark/large.json"
                    (asdf:system-source-directory :parsnip)))
 
-(defun benchmark-decoder (decoder)
-  "Measure the time a decoder reads a jarge json payload many times."
+(defun benchmark-stream-decoder (decoder)
+  "Measure the time a decoder reads a jarge json stream payload many times."
   (with-open-file (stream +large-json-path+)
     (time
       (dotimes (n 100)
         (funcall decoder stream)
         (file-position stream 0)))))
 
+(defun benchmark-string-decoder (decoder)
+  "Measure the time a decoder reads a large json string payload many times."
+  (let ((payload (alexandria:read-file-into-string +large-json-path+)))
+    (time
+      (dotimes (n 100) (funcall decoder payload)))))
+
 (defun benchmark ()
+  (format t "===READING FROM STREAM~%")
   (format t "PARSNIP:~%")
-  (benchmark-decoder #'read-json)
+  (benchmark-stream-decoder #'read-json)
 
   (format t "~%~%~%CL-JSON:~%")
-  (benchmark-decoder #'cl-json:decode-json))
+  (benchmark-stream-decoder #'cl-json:decode-json)
+
+  (format t "~%~%~%===READING FROM STRING===~%")
+  (format t "PARSNIP:~%")
+  (benchmark-string-decoder #'read-json-from-string)
+
+  (format t "~%~%~%Cl-JSON:~%")
+  (benchmark-string-decoder #'cl-json:decode-json-from-string))
 
 (benchmark)
